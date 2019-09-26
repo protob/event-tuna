9b797cf9b554a940e1a9940ade4a493d1b11199d
LG <protobx@gmail.com>
2019-09-27 00:35:46 +0200

    mod date filter

--- components/molecules/toolbar/GeoTimeToolbar.vue
+++ components/molecules/toolbar/GeoTimeToolbar.vue
@@ -1,63 +1,51 @@
 <template>
-  <section class="has-filter geo-time-filter">
-    <v-container class="pt-0 pb-0">
-      <v-row>
-        <v-col class="pt-0 pb-0">
-          <v-flex xs12 sm12>
-            <v-container bg fill-height grid-list-md text-xs-center>
-              <v-layout row wrap align-center>
-                <v-flex xs8>
-                  <v-text-field
-                    class="small-input"
-                    :value="getStartDate"
-                    label="start date"
-                    small
-                    solo
-                    @input="updateStartDate($event.target.value)"
-                    hide-details
-                    single-line
-                    outlined
-                  ></v-text-field>
-                </v-flex>
-                <v-flex xs4>
-                  <start-date />
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-flex>
-        </v-col>
-        <v-col class="pt-0 pb-0">
-          <v-flex xs12 sm12>
-            <v-container bg fill-height grid-list-md text-xs-center>
-              <v-layout row wrap align-center>
-                <v-flex xs8>
-                  <v-text-field
-                    class="small-input"
-                    :value="getEndDate"
-                    label="end date"
-                    solo
-                    hide-details
-                    @input="updateEndDate($event.target.value)"
-                    single-line
-                    outlined
-                  ></v-text-field>
-                </v-flex>
-                <v-flex xs4>
-                  <end-date />
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-flex>
-        </v-col>
-        <v-col>
+  <!---col---->
+  <v-container pa-0 ma-0 class="has-filter geo-time-filter">
+    <v-layout row wrap align-center>
+      <v-flex xs4>
+        <v-container bg pa-0 ma-0 fill-height grid-list-md text-xs-center>
+          <v-text-field
+            class="small-input"
+            :value="getStartDate"
+            label="start date"
+            small
+            solo
+            hide-details
+            single-line
+            outlined
+            @input="updateStartDate($event.target.value)"
+          ></v-text-field>
+          <start-date />
+        </v-container>
+      </v-flex>
+      <!---col---->
+      <v-flex xs4>
+        <v-container bg pa-0 ma-0 fill-height grid-list-md text-xs-center>
+          <v-text-field
+            class="small-input"
+            :value="getEndDate"
+            label="end date"
+            solo
+            hide-details
+            single-line
+            outlined
+            @input="updateEndDate($event.target.value)"
+          ></v-text-field>
+
+          <end-date />
+        </v-container>
+      </v-flex>
+
+      <v-flex xs4>
+        <v-container bg pa-0 ma-0 fill-height grid-list-md text-xs-center>
           <v-btn color="primary" @click="filterEventsByTimeRange">
             <font-awesome-icon :icon="['fas', 'search']"></font-awesome-icon>
           </v-btn>
           <v-btn color="primary" onclick="alert('TODO')">Region</v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </section>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -95,5 +83,8 @@ export default {
 <style lang="scss" scoped>
 .row {
   height: 100%;
+}
+.geo-time-filter {
+  max-width: 500px;
 }
 </style>
